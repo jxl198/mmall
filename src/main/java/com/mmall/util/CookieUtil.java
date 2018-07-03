573eb3086efb606d7d04e63e4d162de5769eaa3b
amina <jxl198@126.com>
2018-07-03 18:02:12 +0800

    modify tomcat8.5

--- src/main/java/com/mmall/util/CookieUtil.java
+++ src/main/java/com/mmall/util/CookieUtil.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpServletResponse;
  **/
 @Slf4j
 public class CookieUtil {
-    private static final String COOKIE_DOMAIN = ".imooc.com";
+    private static final String COOKIE_DOMAIN = "imooc.com";
     private static final String COOKIE_NAME = "mmall_login_token";
 
     /**
